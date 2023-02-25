33638086781f58b4e48b716ca9e97cdab932c921
MubshirM <mubshirmmm@gmail.com>
2023-02-25 05:23:16 -0800

    first commit

---
+++ task 4b.py
@@ -0,0 +1,17 @@
+def first_num(arrange):
+    return arrange[0]
+file=[1,2,3,4,5,6,7,8,9]
+first=first_num(file)
+print(first)
+
+name=[['rashid','ali','noor'],['naman','nadeem','arshad'],['rahman','raheem','rashid']]
+first_name=first_num(name)
+print(first_name)
+
+
+
+#dout
+def join_something(a):
+    return "something " + str(a)
+name=join_something()
+print(name)
